4358c5132d6764ca072134ccdd966366955df3c3
toy <15968477+toyfer@users.noreply.github.com>
2024-04-08 10:03:08 +0900

    initial commit

---
+++ background.js
@@ -0,0 +1,8 @@
+﻿chrome.browserAction.onClicked.addListener(function(tab) {
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs) {
+    var url = tabs[0].url;
+    var targetLang = "ja"; // 目的言語を設定 (例: 英語)
+    var translateUrl = "https://translate.google.com/translate?sl=auto&tl=" + targetLang + "&u=" + encodeURIComponent(url);
+    chrome.tabs.create({ url: translateUrl });
+  });
+});
