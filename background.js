fded7d35892d9d66b7afc61874dad17c17d67e5d
toy <15968477+toyfer@users.noreply.github.com>
2024-08-08 09:18:19 +0900

    [update]manifest v2->manifest v3

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-﻿chrome.browserAction.onClicked.addListener(function(tab) {
+﻿chrome.action.onClicked.addListener(function(tab) {
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs) {
     var url = tabs[0].url;
     var targetLang = "ja"; // 目的言語を設定 (例: 英語)
